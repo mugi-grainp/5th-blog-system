963ed1319db31cbc72ff02ff8a8b078753cc0f8b
mugi-grainp <kawa.jnra415@gmail.com>
2021-06-02 21:16:38 +0900

    記事一覧ページ生成システム実装

---
+++ admin/bin/archive-listpage-generate.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# archive-listpage-generate.sh
+#
+# Usage
+# admin/bin/archive-listpage-generate.sh
+#
+# 引数
+# なし
+#
+# 出力
+# 記事ディレクトリに記事HTMLファイルが出力される
+# 記事ディレクトリに記事Markdownファイルがコピーされる
+# インデックスページが更新される
+
+# 初期化 ------------------------------
+set -u
+umask 0022
+PATH='/usr/bin:/bin'
+IFS=$(printf ' \t\n_'); IFS=${IFS%_}
+export IFS LC_ALL=C LANG=C PATH
+
+Tmp=/tmp/${0##*/}.$$
+# -------------------------------------
+
+# 記事一覧を生成 -------------------------------------------------------------------------
+echo "<table>" > $Tmp-articletable
+echo "<tr><th>日付</th><th>タイトル</th><th>概要</th><td>" >> $Tmp-articletable
+
+awk 'BEGIN { FS = "\t" }
+     {
+         printf "<tr><td>%s</td><td><a href=\"posts/%s/\">%s</a></td><td>%s</td></tr>\n",
+                $1, $2, $3, $4
+     }' $(dirname $0)/../article-list.txt >> $Tmp-articletable
+
+echo "</table>" >> $Tmp-articletable
+# ----------------------------------------------------------------------------------------
+
+# テンプレートにはめ込み -----------------------------------------------------------------
+cat $(dirname $0)/../templates/archives-template.html |
+    sed '/@postslist/r '$Tmp'-articletable'           |
+    sed '/@postslist/d'                               > $Tmp-articlehtml
+# ----------------------------------------------------------------------------------------
+
+# ファイルの置き換え ---------------------------------------------------------------------
+mv $(dirname $0)/../../archives/index.html $(dirname $0)/../../archives/index-prev.html
+cp $Tmp-articlehtml $(dirname $0)/../../archives/index.html
+# ----------------------------------------------------------------------------------------
+
+# 終了処理 ----------------------------
+rm -f $Tmp-*
+# -------------------------------------
+
